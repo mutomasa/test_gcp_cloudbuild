480d72b9c4cbb119e34aa1b3fb3b43a16129323b
Masahiro Muto <mutomasa118@gmail.com>
2020-12-14 11:39:21 +0000

    jwt parse

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module helloworld
 
 go 1.12
+
+require (
+	golang.org/x/crypto v0.0.0-20201016220609-9e8e0b390897 // indirect
+	gopkg.in/dgrijalva/jwt-go.v3 v3.2.0 // indirect
+	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
+)
--- main.go
+++ main.go
@@ -11,13 +11,75 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		jwtHeaderName := "x-goog-iap-jwt-assertion"
+		//公開鍵のURL
+		jwkUrl := "https://www.gstatic.com/iap/verify/public_key-jwk"
+		// issuerのURL
+		issuerUrl := "https://cloud.google.com/iap"
+		//audienceの値
+		audience := "/projects/1013152892072/apps/phonic-axle-282407"
 
 		//HTTP ヘッダからJWTを取得
 		tokenString := r.Header.Get(jwtHeaderName)
-		w.WriteHeader(200)
+		//公開鍵を取得
+		resp, err = http.Get(jwkUrl)
+		defer resp.Body.Close()
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w,err)
+			return
+		}
 
-		//JWTをそのまま表示
-		fmt.Fprintln(w, tokenString)
+		//公開鍵読み取り
+		keyBody, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w,err)
+			return
+		}
+
+		token, err := jwt.Parse(tokenString,func(token *jwt.Token)(interface{},error){
+			//署名アルゴリズムの検証
+			if _ ,ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+				return nil,fmt.Errorf("unexpected signing method: %v",token.Header["alg"])
+			}
+
+			//JWT の情報(Claims)の取得
+			claims := token.Claims.(jwt.MapClaims)
+			//audience ,issuerの検証
+			if claims["iss"] != issuerUrl {
+				return nil,fmt.Errorf("Invalid issuer:%v",claims["iss"])
+			}
+
+			//audienceの検証
+			if claims["aud"] != audience {
+				return nil,fmt.Errorf("invalid auduence: %v",claims["aud"])
+
+			}
+
+			//公開鍵をパース
+			var keySet jose.JSONWebKeySet
+			err := json.Unmarshal(keybody,&keySet)
+
+			//複数の公開鍵からトークンの"kid"に合致する公開鍵を返却
+			kid := token.Header["kid"].(string)
+			return keySet.Key(kid)[0].Key, err
+		})
+
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w,err)
+			return
+		}
+
+		//サブジェクトとメールアドレスを取得
+		claims := token.Claims.(jwt.MapClaims)
+		email := claims["email"]
+		subject := claims["sub"]
+
+		w.WriteHeader(200)
+		//サブジェクトとメールアドレスを表示する
+		fmt.Fprinln(w,email)
+		fmt.Fprintln(w,subject)
 	})
 
 	port := os.Getenv("PORT")
